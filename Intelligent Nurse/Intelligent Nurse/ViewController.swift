fabc4f2b06a0b9b457ffd5930bd3aabb56bb03df
Daria Firsova <tonitruif@gmail.com>
2018-10-31 01:17:11 +0300

    initial commit project, podfile

---
+++ Intelligent Nurse/Intelligent Nurse/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Intelligent Nurse
+//
+//  Created by  Daria Firsova on 30/10/2018.
+//  Copyright © 2018  Daria Firsova. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Intelligent Nurse/Pods/Target Support Files/Pods-Intelligent Nurse/Pods-Intelligent Nurse-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Intelligent_Nurse : NSObject
+@end
+@implementation PodsDummy_Pods_Intelligent_Nurse
+@end
