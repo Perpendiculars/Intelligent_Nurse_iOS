b81926461f783f34cc807caa2bc440bb16e6eddb
Daria Firsova <tonitruif@gmail.com>
2018-11-27 02:19:17 +0300

    Basic ui and outlets

---
+++ Intelligent Nurse/Intelligent Nurse/LoginViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LoginViewController.swift
+//  Intelligent Nurse
+//
+//  Created by  Daria Firsova on 27/11/2018.
+//  Copyright © 2018  Daria Firsova. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController
+{
+    @IBAction func LoginButton(_ sender: UIButton) {
+    }
+    
+    @IBOutlet weak var LoginText: UITextField!
+    @IBOutlet weak var PasswordText: UITextField!
+    @IBOutlet weak var TokenText: UITextField!
+}
+
---
+++ Intelligent Nurse/Intelligent Nurse/SignUpViewController.swift
@@ -0,0 +1,21 @@
+//
+//  SignUpViewController.swift
+//  Intelligent Nurse
+//
+//  Created by  Daria Firsova on 27/11/2018.
+//  Copyright © 2018  Daria Firsova. All rights reserved.
+//
+
+
+import UIKit
+
+class SignUpViewController: UIViewController
+{
+    
+    @IBOutlet weak var PasswordText: UITextField!
+    @IBOutlet weak var LoginText: UITextField!
+    @IBOutlet weak var TokenText: UITextField!
+    
+    @IBAction func SignUpButton(_ sender: UIButton) {
+    }
+}
