62bee3cbb66b300f1645fe52f3080993b23505c3
Марк <hartmann@MacBook-Pro-Mark.local>
2018-12-05 20:57:33 +0300

    add browser

--- Intelligent Nurse/Intelligent Nurse/ViewController.swift
+++ Intelligent Nurse/Intelligent Nurse/ViewController.swift
@@ -7,19 +7,17 @@
 //
 
 import UIKit
-
+import WebKit
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var WebView: WKWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        let url = URL(string: "https://vk.com")
+        let request = URLRequest(url: url!)
+        WebView.load(request)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
 
